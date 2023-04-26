4d241521f8126d6c765d3c1fea51f18392ea817c
Divya Gurung <divyagurung@Divyas-Air.mshome.net>
2023-04-27 00:35:50 +0530

    making first changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
-
+import search from "./search.png"
+import './config.js'
+import db from './config.js';
+import { doc, getDocs, collection, getFirestore } from "firebase/firestore";
+import { useEffect, useState } from 'react';
 function App() {
+  const [document, setDocument] = useState([]);
+  useEffect(() => {
+    getData();
+  }, [])
+
+  const getData = async () => {
+    const querySnapshot = await getDocs(collection(db, "books"));
+    querySnapshot.forEach((doc) => {
+      setDocument(r => [...r, doc.data()]);
+      console.log(document)
+
+    });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='background'>
+        <div className='background-cover'>
+          <h2>BIBLIOPHILE:ONE STOP SOLUTION FOR ALL YOUR BOOKISH NEEDS</h2>
+          <div className='search-wrapper'>
+            <div className='search-container'>
+              <input type="search" placeholder='Search your favorite book' className='search-bar' />
+              <img src={search} width="20px" height={"20px"}
+                style={{ marginRight: "20px" }} />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div>
+        <h3>Here are your books</h3>
+        <div className='card-scroll-horizontal'>
+          {document && document.map((item, index) => (
+            <div className='book-card-container' key={index}>
+              <div className='book-card-img-container'>
+                <img src={item.imageUrl} className="book-img" alt={doc.name} />
+              </div>
+              <div>
+                <p>{item.name}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+
+
+      </div>
     </div>
   );
 }
---
+++ src/config.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "@firebase/firestore";
+const firebaseConfig = {
+    apiKey: "AIzaSyDF_uRUI6hAATU6Lukv81kXjQ75qowzIHk",
+    authDomain: "fir-2-7c2e8.firebaseapp.com",
+    projectId: "fir-2-7c2e8",
+    storageBucket: "fir-2-7c2e8.appspot.com",
+    messagingSenderId: "890934481757",
+    appId: "1:890934481757:web:04f48a69960cdba470ceb4",
+    measurementId: "G-NZ7SD6Y17T"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore();
+export default db;
